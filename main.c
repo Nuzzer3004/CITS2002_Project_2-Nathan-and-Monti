c8997dc6b69ab8785ba715ca249e56bb1454dc7d
nathannguyen <nathan0761@gmail.com>
2022-10-02 21:16:56 +0800

    New

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+//
+// CITS2002 Project 1 2022
+// Student1: 23057892 Chambers Monti
+// Student2: 23058262 Nguyen Nathan
+//
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
